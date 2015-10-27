3970c364ac7a7994e4eff119735a5ccfdb02d56c
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-10-27 10:58:04 +0000

    end of chapter 12

--- app.rb
+++ app.rb
@@ -12,8 +12,13 @@ get '/secret/what_up' do
   "yo"
 end
 
-get '/cat' do
-"<div style='border: 3px dashed red'>
-<img src='http://bit.ly/1eze8aE'>
-</div>"
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
